0e6e6ed28c9f3d16ab728a847fbd575f675b0586
aadriaan200418 <133753866+aadriaan200418@users.noreply.github.com>
2025-10-31 11:54:33 +0100

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white p-4 text-center">
-      © 2025 Cineverse. Todos los derechos reservados.
-    </footer>
-  );
-}
-
-export default Footer;
