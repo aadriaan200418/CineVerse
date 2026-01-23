88ed9d31a05728b6e0ca911aac50427d6a152030
sofiarodriguezh <sofia.rodrigez82@educa.madrid.org>
2026-01-23 10:47:09 +0100

    finiquitao

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -47,6 +47,7 @@ export default function Login() {
         localStorage.setItem("role", data.role);
         localStorage.setItem("token", data.token);
         localStorage.setItem("dni", data.id_profile);
+        localStorage.setItem("id_profile", data.id_profile); 
         if (data.role === "admin") {
           navigate("/home");
         }
